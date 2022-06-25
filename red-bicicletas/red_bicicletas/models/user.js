b35e769d2b89fc55f98c7f72a470685f936f27ca
ignaciocabrera <igncabrera25@gmail.com>
2022-06-25 19:48:08 -0300

    token

--- red-bicicletas/red_bicicletas/models/user.js
+++ red-bicicletas/red_bicicletas/models/user.js
@@ -1,10 +1,55 @@
 var mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
 var Booking =  require('./booking');
 var Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+
+
+
+const validateMail =  function(email){
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+}
 
 var userSchema = new Schema({
-    name: String,
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'Email is required'],
+        lowercase: true,
+        unique: true,
+        validate: [validateMail, 'Enter a valid email'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String, 
+        required: [true, 'Password is required']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verified: {
+        type: Boolean, 
+        default: false
+    }
 })
+
+userSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds)
+    }
+})
+
+userSchema.plugin(uniqueValidator, {message: 'This {PATH} has an account already'})
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 
 userSchema.methods.booking = function(bikeId, from, to, cb){
     var book = new Booking({user: this._id, bike: bikeId, from: from, to: to})
@@ -13,3 +58,4 @@ userSchema.methods.booking = function(bikeId, from, to, cb){
 }
 
 module.exports = mongoose.model('User', userSchema);
+
