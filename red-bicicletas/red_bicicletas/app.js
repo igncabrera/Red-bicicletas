57af8785677fedd6dc47d3dd080129dcf7b4f560
ignaciocabrera <igncabrera25@gmail.com>
2022-06-26 05:28:17 -0300

    fix

--- red-bicicletas/red_bicicletas/app.js
+++ red-bicicletas/red_bicicletas/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var tokenRouter = require('./routes/token')
 var usersRouter = require('./routes/users');
 var bikesRouter = require('./routes/bikes');
 var bikesAPIRouter = require('./routes/api/bikes');
@@ -32,9 +33,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/token', tokenRouter);
 app.use('/bikes', bikesRouter);
 app.use('/api/bikes', bikesAPIRouter);
-app.use('/api/users', usersAPIRouter)
+app.use('/api/users', usersAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- red-bicicletas/red_bicicletas/models/user.js
+++ red-bicicletas/red_bicicletas/models/user.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator')
 var Booking =  require('./booking');
 var Schema = mongoose.Schema
+var Token = require('../models/token');
+var mailer = require('../mailer/mailer')
+var crypto = require('crypto')
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
 
@@ -55,6 +58,26 @@ userSchema.methods.booking = function(bikeId, from, to, cb){
     var book = new Booking({user: this._id, bike: bikeId, from: from, to: to})
     console.log(book);
     book.save(cb);
+}
+
+userSchema.methods.send_welcome_email = function(cb){
+    const token = new Token({_userId: this.id, token: crypto.randomBytes(16).toString('hex')});
+    const email_destination = this.email;
+    token.save(function(err){
+        if(err){return console.log(err.message);}
+
+        const mailOptions = {
+            from: 'no-reply@redbicicletas.com',
+            to: email_destination,
+            subject: 'Account verification',
+            text: 'Hola, \n\n' + 'Por favor, para verificar su cuenta haga click en este enlace: \n' + 'http:localhost:5000' + '\/token/confirmation\/' + token.token + '.\n'
+        }
+
+        mailer.sendMail(mailOptions, function(err){
+            if(err){ return console.log(err, message);}
+            console.log('Se ha enviado un mal de bienvenida a:'+ email_destination + '.');
+        })
+    })
 }
 
 module.exports = mongoose.model('User', userSchema);
